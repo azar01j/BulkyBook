ec16069b8ef971577e1b20d1dc6414b76e6d0800
scs <scs@AZAR>
2023-02-20 17:29:41 -0500

    Add project files.

---
+++ BulkyBookLibrary/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyBookLibrary.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string Name { get; set; } 
+    public int DisplayOrder { get; set; } 
+
+    public DateTime CreatedDate { get; set; } =  DateTime.Now;
+
+}
