b9623c83a328fe63b903965d9af6817f85382520
azar01j <106423081+azar01j@users.noreply.github.com>
2023-02-20 22:50:58 -0500

    DB Context created

---
+++ BulkyBookLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookLibrary.Data;
+
+
+public class ApplicationDbContext :DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+	{
+
+	}
+
+	public DbSet<Category> Categories { get; set; }
+}
--- BulkyBookLibrary/Models/Category.cs
+++ BulkyBookLibrary/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyBookLibrary.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookLibrary.Models;
 
 public class Category
 {
+    [Key]
     public int CategoryId { get; set; }
+    [Required]
     public string Name { get; set; } 
     public int DisplayOrder { get; set; } 
 
