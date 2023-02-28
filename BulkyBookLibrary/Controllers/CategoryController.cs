fdd66451cff65b5e10f1cc830f44f5e9804f0a93
azar01j <106423081+azar01j@users.noreply.github.com>
2023-02-28 16:14:34 -0500

    Create Fucntion added

--- BulkyBookLibrary/Controllers/CategoryController.cs
+++ BulkyBookLibrary/Controllers/CategoryController.cs
@@ -17,5 +17,22 @@ namespace BulkyBookLibrary.Controllers
             IEnumerable<Category> objCategoryList =  _db.Categories.ToList();
             return View(objCategoryList);
         }
+        
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
