7a7d636b3ddf0d172874e468195d488363f86d75
azar01j <106423081+azar01j@users.noreply.github.com>
2023-03-01 20:24:05 -0500

    Edit/Delete function added

--- BulkyBookLibrary/Controllers/CategoryController.cs
+++ BulkyBookLibrary/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBookLibrary.Data;
 using BulkyBookLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Remoting;
 
 namespace BulkyBookLibrary.Controllers
 {
@@ -41,5 +43,76 @@ namespace BulkyBookLibrary.Controllers
             }
             return View(obj);
         }
+        
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromDb = _db.Categories.Find(id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder and Name can not be the same value!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromDb = _db.Categories.Find(id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? categoryid)
+        {
+            var obj = _db.Categories.Find(categoryid);
+            /*Console.Read(obj);*/
+            /*Console.WriteLine(obj);
+*/            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+         
+        }
+
     }
 }
--- BulkyBookLibrary/Models/Category.cs
+++ BulkyBookLibrary/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookLibrary.Models;
 
@@ -7,7 +8,10 @@ public class Category
     [Key]
     public int CategoryId { get; set; }
     [Required]
-    public string Name { get; set; } 
+    public string Name { get; set; }
+    
+    [DisplayName("Display Order")]
+    [Range(1,20,ErrorMessage ="Display Order Should be between 1 to 100 !")]
     public int DisplayOrder { get; set; } 
 
     public DateTime CreatedDate { get; set; } =  DateTime.Now;
